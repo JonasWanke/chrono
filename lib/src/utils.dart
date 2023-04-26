5f121008afe13168206174bfe193b2c034b9bd29
Jonas Wanke <contact@jonas-wanke.com>
2023-04-26 15:46:37 +0200

    Add fixed.secondsAsMicroseconds

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,6 +1,16 @@
+import 'package:fixed/fixed.dart';
+
 mixin ComparisonOperatorsFromComparable<T> implements Comparable<T> {
   bool operator <(T other) => compareTo(other) < 0;
   bool operator <=(T other) => compareTo(other) <= 0;
   bool operator >(T other) => compareTo(other) > 0;
   bool operator >=(T other) => compareTo(other) >= 0;
+}
+
+extension FixedPlainDateTimeInternal on Fixed {
+  int get secondsAsMicroseconds {
+    // `Fixed`'s `*` operator performs rounding to a `double` internally,
+    // causing precision loss.
+    return Fixed.copyWith(this, scale: 6).minorUnits.toInt();
+  }
 }
