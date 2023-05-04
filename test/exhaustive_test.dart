7550da5a3fec02ee922d99769480ee8f5ea871c9
Jonas Wanke <contact@jonas-wanke.com>
2023-05-04 20:17:20 +0200

    Test plainDate.dayOfYear

--- test/exhaustive_test.dart
+++ test/exhaustive_test.dart
@@ -4,7 +4,9 @@ import 'package:supernova/supernova.dart' hide Instant, Weekday;
 
 void main() {
   test('Exhaustive arithmetic check for chrono', () {
+    // Inspired by this website, plus some extra tests:
     // https://howardhinnant.github.io/date_algorithms.html#Yes,%20but%20how%20do%20you%20know%20this%20all%20really%20works?
+
     final unixEpoch =
         PlainDate.fromThrowing(const PlainYear(1970), PlainMonth.january, 1);
     expect(
@@ -29,6 +31,8 @@ void main() {
       PlainMonth.january,
       1,
     );
+    final startDateDaysSinceUnixEpoch = startDate.daysSinceUnixEpoch;
+
     const endYear = -startYear;
     final endDate = PlainDate.fromThrowing(
       const PlainYear(endYear),
@@ -40,10 +44,16 @@ void main() {
         endDate.daysSinceUnixEpoch - startDate.daysSinceUnixEpoch + 1;
     // expect(totalNumberOfDays, 730485366);
 
-    var previousDaysSinceUnixEpoch = startDate.daysSinceUnixEpoch - 1;
-    // expect(previousDaysSinceUnixEpoch, -365962029);
-    var previousWeekday =
-        PlainDate.fromDaysSinceUnixEpoch(previousDaysSinceUnixEpoch).weekday;
+    final previousDate =
+        PlainDate.fromDaysSinceUnixEpoch(startDateDaysSinceUnixEpoch - 1);
+    var previous = (
+      daysSinceUnixEpoch: startDateDaysSinceUnixEpoch - 1,
+      dayOfYear: previousDate.dayOfYear,
+      weekday: previousDate.weekday,
+    );
+    // expect(previous.daysSinceUnixEpoch, -365962029);
+    // expect(previous.dayOfYear, TODO);
+    // expect(previous.weekday, TODO);
 
     final startTime = Instant.now();
     for (var year = startYear; year <= endYear; ++year) {
@@ -53,16 +63,26 @@ void main() {
           final date = PlainDate.fromYearMonthAndDayThrowing(yearMonth, day);
 
           final daysSinceEpoch = date.daysSinceUnixEpoch;
-          expect(daysSinceEpoch, previousDaysSinceUnixEpoch + 1);
-
+          expect(daysSinceEpoch, previous.daysSinceUnixEpoch + 1);
           expect(date, PlainDate.fromDaysSinceUnixEpoch(daysSinceEpoch));
 
-          final weekday = date.weekday;
-          expect(previousWeekday.next, weekday);
-          expect(weekday.previous, previousWeekday);
+          final dayOfYear = date.dayOfYear;
+          expect(
+            dayOfYear,
+            date.month == PlainMonth.january && date.day == 1
+                ? 1
+                : previous.dayOfYear + 1,
+          );
 
-          previousDaysSinceUnixEpoch = daysSinceEpoch;
-          previousWeekday = weekday;
+          final weekday = date.weekday;
+          expect(previous.weekday.next, weekday);
+          expect(weekday.previous, previous.weekday);
+
+          previous = (
+            daysSinceUnixEpoch: daysSinceEpoch,
+            dayOfYear: dayOfYear,
+            weekday: weekday,
+          );
         }
       }
     }
