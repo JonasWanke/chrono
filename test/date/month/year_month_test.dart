e6044419ceb4ac0dde7cec4e98bc0e44fbc7a111
Jonas Wanke <contact@jonas-wanke.com>
2023-07-16 16:42:38 +0200

    Test YearMonth

---
+++ test/date/month/year_month_test.dart
@@ -0,0 +1,21 @@
+import 'package:chrono/chrono.dart';
+import 'package:glados/glados.dart';
+
+import '../../utils.dart';
+
+void main() {
+  setChronoGladosDefaults();
+
+  testDataClassBasics(YearMonth.fromJson);
+
+  Glados<YearMonth>().test('lengthInDays', (yearMonth) {
+    expect(yearMonth.lengthInDays, inInclusiveRange(28, 31));
+  });
+
+  Glados<YearMonth>().test('next and previous', (yearMonth) {
+    expect(yearMonth.next.previous, yearMonth);
+  });
+  Glados2<YearMonth, MonthsDuration>().test('+ and -', (yearMonth, duration) {
+    expect(yearMonth + duration - duration, yearMonth);
+  });
+}
