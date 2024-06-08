94b2060d18dedfaae360a1bbaf815939f8cc751c
Jonas Wanke <contact@jonas-wanke.com>
2024-06-08 17:29:53 +0200

    Update Dart to ^3.4.0

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # Dart 3.4.2: https://github.com/NixOS/nixpkgs/commit/483914124e9ec4808e97d444b280188fc59ea0b9
+    nixpkgs.url =
+      "github:nixos/nixpkgs?ref=483914124e9ec4808e97d444b280188fc59ea0b9";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
