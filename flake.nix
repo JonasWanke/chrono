d5ce8a530e95fdd3216cbe3dfa33229b9ad219e8
Jonas Wanke <contact@jonas-wanke.com>
2025-07-26 17:54:01 +0200

    Upgrade to Dart 3.8

--- flake.nix
+++ flake.nix
@@ -1,13 +1,22 @@
 {
   inputs = {
-    # https://github.com/NixOS/nixpkgs/pull/364980
-    nixpkgs.url =
-      "github:nixos/nixpkgs?ref=711e659590895f1de0ada40dbdc1eb5bae98a179";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in { devShell = with pkgs; mkShell { buildInputs = [ dart ]; }; });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell { buildInputs = [ dart ]; };
+      }
+    );
 }
