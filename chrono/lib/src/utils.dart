79c867fd285cafbd24ae393ee7df35fd315b2870
Jonas Wanke <contact@wanke.dev>
2026-06-25 13:38:59 +0200

    Add @internal annotations to utils

--- chrono/lib/src/utils.dart
+++ chrono/lib/src/utils.dart
@@ -1,3 +1,6 @@
+import 'package:meta/meta.dart';
+
+@internal
 mixin ComparisonOperatorsFromComparable<T> implements Comparable<T> {
   bool operator <(T other) => compareTo(other) < 0;
   bool operator <=(T other) => compareTo(other) <= 0;
@@ -5,6 +8,7 @@ mixin ComparisonOperatorsFromComparable<T> implements Comparable<T> {
   bool operator >=(T other) => compareTo(other) >= 0;
 }
 
+@internal
 extension LetExtension<T extends Object> on T {
   R let<R>(R Function(T) block) => block(this);
 }
